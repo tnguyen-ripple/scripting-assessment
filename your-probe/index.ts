5bfc8338805ea435eb39a001510f1b54bf51ba45
Thu-Trang Nguyen <tnguyen@ripple.com>
2024-12-12 19:38:07 +0800

    from my understanding

--- your-probe/index.ts
+++ your-probe/index.ts
@@ -3,10 +3,27 @@ import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { GetLastBlock, NodeBody } from "./src/client";
 
+const collectorOptions = {
+    headers: {}, // an optional object containing custom headers to be sent with each request
+    concurrencyLimit: 1, // an optional limit on pending requests
+};
+const metricExporter = new OTLPMetricExporter(collectorOptions);
+const meterProvider = new MeterProvider({});
 
+meterProvider.addMetricReader(new PeriodicExportingMetricReader({
+    exporter: metricExporter,
+    exportIntervalMillis: 1000,
+}));
 
+const meter = meterProvider.getMeter('eth-public-node');
+const counter = meter.createCounter('latest_block_number', {
+    description: 'Latest hexa block number of ETH Node',
+  });
+  
 const start = async () => {
-    // have your code here
+  const latestBlock = await GetLastBlock();
+  counter.add(latestBlock);
+  return 
 };
 
 start();
