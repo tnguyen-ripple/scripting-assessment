93aed3267c66707fac4749bf26494055475add86
Thu-Trang Nguyen <tnguyen@ripple.com>
2024-12-12 20:16:10 +0800

    loop in async function

--- your-probe/index.ts
+++ your-probe/index.ts
@@ -3,6 +3,9 @@ import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { GetLastBlock, NodeBody } from "./src/client";
 
+
+console.log("testing testing");
+
 const collectorOptions = {
     headers: {}, // an optional object containing custom headers to be sent with each request
     concurrencyLimit: 1, // an optional limit on pending requests
@@ -19,11 +22,17 @@ const meter = meterProvider.getMeter('eth-public-node');
 const counter = meter.createCounter('latest_block_number', {
     description: 'Latest hexa block number of ETH Node',
   });
-  
+
+
 const start = async () => {
-  const latestBlock = await GetLastBlock();
-  counter.add(latestBlock);
-  return 
+    while(true) {
+        const lastBlockNumber = await GetLastBlock();
+        counter.add(lastBlockNumber);
+        console.log(lastBlockNumber);
+
+        // wait for 5 seconds before pulling new number
+        await new Promise(resolve => setTimeout(resolve, 5000)); 
+    }
 };
 
 start();
